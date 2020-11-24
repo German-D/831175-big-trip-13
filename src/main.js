f7a77195e9d2df9ff0add55b263bc9ffc36ea6b4
German Dolnikov <german@dolnikov.ru>
2020-11-24 23:06:20 +0300

    Время разбивать камни

---
+++ src/main.js
@@ -0,0 +1,29 @@
+
+import {createEventInfoTemplate} from "./view/event-info.js";
+import {createMenuTemplate} from "./view/menu.js";
+import {createFiltersTemplate} from "./view/filters.js";
+import {createSortTemplate} from "./view/sort.js";
+import {createEventFormTemplate} from "./view/event-form.js";
+import {creatEventPointTemplate} from "./view/event-point.js";
+
+const POINT_COUNT = 3;
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const tripMain = document.querySelector(`.trip-main`);
+render(tripMain, createEventInfoTemplate(), `beforeend`);
+
+const tripControls = document.querySelector(`.trip-controls`);
+render(tripControls, createMenuTemplate(), `beforeend`);
+render(tripControls, createFiltersTemplate(), `beforeend`);
+
+
+const tripEvents = document.querySelector(`.trip-events`);
+render(tripEvents, createSortTemplate(), `beforeend`);
+render(tripEvents, createEventFormTemplate(), `beforeend`);
+
+for (let i = 0; i < POINT_COUNT; i++) {
+  render(tripEvents, creatEventPointTemplate(), `beforeend`);
+}
